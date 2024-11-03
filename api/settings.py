7e4197c660f44ac2fd30c5fd0b38f8d7076ac6c3
José Miguel Quintana <josemiguelquinta@gmail.com>
2024-11-03 19:12:25 +0100

    feat: remove middleware

--- api/settings.py
+++ api/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "api.middlewares.jwt_middleware.JWTAuthenticationMiddleware",
 ]
 
 ROOT_URLCONF = "api.urls"
