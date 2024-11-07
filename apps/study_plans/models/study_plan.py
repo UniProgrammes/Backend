c50ac53d9ba40541f3a714ff288f379efb618313
hajizadeh2079 <hajizadeh2079@gmail.com>
2024-11-07 19:56:38 +0100

    Resolve conflicts

--- apps/study_plans/models/study_plan.py
+++ apps/study_plans/models/study_plan.py
@@ -1,14 +1,19 @@
-from django.db import models
-from django.db.models import CharField, ForeignKey, ManyToManyField
+from django.db.models import CharField, DateTimeField, CASCADE, ForeignKey, ManyToManyField
 
 from apps.lib.models import UUIDModel
 
 
 class StudyPlan(UUIDModel):
+    STATUS_CHOICES = [
+        ("draft", "Draft"),
+        ("completed", "Completed"),
+    ]
     name = CharField(max_length=255)
-    status = CharField(max_length=255)
+    status = CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
     user = ForeignKey(
-        "users.User", on_delete=models.CASCADE, related_name="study_plans"
+        "users.User", on_delete=CASCADE, related_name="study_plans"
     )
     courses = ManyToManyField(
         "courses.Course",
