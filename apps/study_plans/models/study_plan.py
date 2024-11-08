7963841d0535a387e322c77a79711720a71649e9
José Miguel Quintana <josemiguelquinta@gmail.com>
2024-11-08 13:03:23 +0100

    fix: delete unnecessary datetimes

--- apps/study_plans/models/study_plan.py
+++ apps/study_plans/models/study_plan.py
@@ -1,4 +1,9 @@
-from django.db.models import CharField, DateTimeField, CASCADE, ForeignKey, ManyToManyField
+from django.db.models import (
+    CharField,
+    CASCADE,
+    ForeignKey,
+    ManyToManyField,
+)
 
 from apps.lib.models import UUIDModel
 
@@ -10,11 +15,7 @@ class StudyPlan(UUIDModel):
     ]
     name = CharField(max_length=255)
     status = CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
-    user = ForeignKey(
-        "users.User", on_delete=CASCADE, related_name="study_plans"
-    )
+    user = ForeignKey("users.User", on_delete=CASCADE, related_name="study_plans")
     courses = ManyToManyField(
         "courses.Course",
         related_name="study_plans",
