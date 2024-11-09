e77db4df4ddf1338e1617f3d65821ec31fada5eb
hajizadeh2079 <hajizadeh2079@gmail.com>
2024-11-09 22:17:06 +0100

    Add my-study-plans API

--- apps/study_plans/serializers.py
+++ apps/study_plans/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from apps.study_plans.models import StudyPlan
 
@@ -7,3 +7,14 @@ class StudyPlanSerializer(ModelSerializer):
     class Meta:
         model = StudyPlan
         fields = "__all__"
+
+
+class StudyPlanSummarySerializer(ModelSerializer):
+    num_courses = SerializerMethodField()
+
+    class Meta:
+        model = StudyPlan
+        fields = ["name", "status", "num_courses", "created_at", "updated_at"]
+
+    def get_num_courses(self, obj):
+        return obj.courses.count()
--- apps/study_plans/views.py
+++ apps/study_plans/views.py
@@ -1,11 +1,14 @@
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from apps.lib.pagination import StandardPagination
 from apps.study_plans.models import StudyPlan
-from apps.study_plans.serializers import StudyPlanSerializer
+from apps.study_plans.serializers import StudyPlanSerializer, StudyPlanSummarySerializer
 
 
 class StudyPlanViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
@@ -15,3 +18,16 @@ class StudyPlanViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
 
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=["get"], url_path="my-study-plans")
+    def my_study_plans(self, request):
+        user = request.user
+        status = request.query_params.get("status")
+
+        if status:
+            study_plans = StudyPlan.objects.filter(user=user, status=status)
+        else:
+            study_plans = StudyPlan.objects.filter(user=user)
+
+        study_plans_data = StudyPlanSummarySerializer(study_plans, many=True).data
+        return Response(study_plans_data, status=HTTP_200_OK)
