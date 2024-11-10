2c26234af7e22139f0c37a7e771b5bfddf79622d
José Miguel Quintana <josemiguelquinta@gmail.com>
2024-11-10 12:44:15 +0100

    feat: user retrieve and update

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -15,7 +15,7 @@ class UserSerializer(ModelSerializer):
             "enrollment_number",
             "password",
         )
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {"password": {"write_only": True}, "id": {"read_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from apps.users.models import User
 from apps.users.serializers import UserSerializer
@@ -10,6 +12,10 @@ from apps.users.serializers import UserSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        return self.queryset.filter(id=self.request.user.id)
 
     @action(detail=False, methods=["post"])
     def register(self, request):
@@ -18,3 +24,18 @@ class UserViewSet(ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    @action(
+        detail=False, methods=["get", "patch"], permission_classes=[IsAuthenticated]
+    )
+    def me(self, request, *args, **kwargs):
+        instance = request.user
+        if request.method == "PATCH":
+            serializer = self.get_serializer(instance, data=request.data, partial=True)
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+            serializer.save()
+        else:
+            serializer = self.get_serializer(instance)
+
+        return Response(serializer.data)
