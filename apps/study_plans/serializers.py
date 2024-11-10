e93acefb0b8f46c8f9f55548b7c1e4a8f37d5d8e
José Miguel Quintana <josemiguelquinta@gmail.com>
2024-11-10 12:26:05 +0100

    feat: study plan crud

--- apps/study_plans/serializers.py
+++ apps/study_plans/serializers.py
@@ -7,3 +7,8 @@ class StudyPlanSerializer(ModelSerializer):
     class Meta:
         model = StudyPlan
         fields = "__all__"
+        read_only_fields = ["user"]
+
+    def create(self, validated_data):
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
--- apps/study_plans/views.py
+++ apps/study_plans/views.py
@@ -1,5 +1,11 @@
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
+from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -8,10 +14,20 @@ from apps.study_plans.models import StudyPlan
 from apps.study_plans.serializers import StudyPlanSerializer
 
 
-class StudyPlanViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
+class StudyPlanViewSet(
+    GenericViewSet,
+    ListModelMixin,
+    RetrieveModelMixin,
+    CreateModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+):
     queryset = StudyPlan.objects.all()
     serializer_class = StudyPlanSerializer
     pagination_class = StandardPagination
 
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
