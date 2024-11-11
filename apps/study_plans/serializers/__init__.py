41bdf6986748a5db908cf65b6ee45b19eda6a91a
José Miguel Quintana <josemiguelquinta@gmail.com>
2024-11-11 11:27:09 +0100

    fix: imports

--- apps/study_plans/serializers/__init__.py
+++ apps/study_plans/serializers/__init__.py
@@ -1,2 +1,5 @@
-from apps.study_plans.serializers.study_plan import StudyPlanSerializer
+from apps.study_plans.serializers.study_plan import (
+    StudyPlanSerializer,
+    StudyPlanSummarySerializer,
+)
 from apps.study_plans.serializers.study_plan_course import StudyPlanCourseSerializer
