36821a3c13be4871907704833ec6b22b4c702a68
Satvik Sharma <satviksharma@Satviks-MacBook-Air.local>
2024-11-19 00:16:41 +0100

    updated URL

--- apps/programmes/urls.py
+++ apps/programmes/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.programmes.views import ProgrammeViewSet, ProgramCoursesView
+from apps.programmes.views import ProgrammeViewSet
 
 router = DefaultRouter()
 router.register("programmes", ProgrammeViewSet, basename="programme")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('programmes/<uuid:uuid>/courses/', ProgramCoursesView.as_view(), name='programme-courses')
+    path('programmes/<uuid:uuid>/courses/', ProgrammeViewSet, name='programme-courses')
 ]
