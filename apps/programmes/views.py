8263e031af40372186d7cdc70a0930b7494c9fac
Satvik Sharma <satviksharma@Satviks-MacBook-Air.local>
2024-11-19 00:20:18 +0100

    remove pylint issues

--- apps/programmes/views.py
+++ apps/programmes/views.py
@@ -1,11 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import GenericViewSet
+from rest_framework import status
+from rest_framework.authentication import JWTAuthentication
+from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
+
 from apps.lib.pagination import StandardPagination
 from apps.programmes.models import Programme
 from apps.programmes.serializers import ProgrammeSerializer
@@ -25,13 +26,13 @@ class ProgrammeViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     filterset_class = ProgrammeFilter
 
     @action(detail=True, methods=['get'], url_path='courses')
-    def courses(self, request, uuid=None):
+    def courses(self, request, **kwargs):
         programme = self.get_object()
         courses = programme.courses.all()
-        
+
         response_data = {
             'programme': ProgrammeSerializer(programme).data,
             'courses': CourseSerializer(courses, many=True).data
         }
-        
+
         return Response(response_data, status=status.HTTP_200_OK)
