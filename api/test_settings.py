8f3273275a963451d239791b703232ddb3013c08
Peppisparrow <127871840+Peppisparrow@users.noreply.github.com>
2024-11-23 21:52:24 +0100

    fix duplicated

--- api/test_settings.py
+++ api/test_settings.py
@@ -1,26 +1,5 @@
 from .settings import *
 
-INSTALLED_APPS = [
-    "corsheaders",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django_filters",
-    "rest_framework",
-    "rest_framework_simplejwt",
-    "apps.health",
-    "apps.programmes",
-    "apps.degrees",
-    "apps.learning_outcomes",
-    "apps.lib",
-    "apps.courses",
-    "apps.users",
-    "apps.study_plans",
-]
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
