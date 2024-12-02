adb02bbde1b337788fb06e145dfb41d0917126ab
hajizadeh2079 <hajizadeh2079@gmail.com>
2024-12-02 16:30:57 +0100

    Use of TabularInline

--- apps/degrees/admin.py
+++ apps/degrees/admin.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from apps.degrees.models import Degree, DegreeProgramme
 
 
+class DegreeProgrammeInline(admin.TabularInline):
+    model = DegreeProgramme
+    extra = 1
+    fields = ("programme", "period_years")
+    autocomplete_fields = ["programme"]
+
+
 @admin.register(Degree)
 class DegreeAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
-    filter_horizontal = ("programmes",)
-
-
-@admin.register(DegreeProgramme)
-class DegreeProgrammeAdmin(admin.ModelAdmin):
-    list_display = ("degree", "programme", "period_years")
-    search_fields = ("degree__name", "programme__name")
-    list_filter = ("period_years",)
+    inlines = [DegreeProgrammeInline]
--- apps/programmes/admin.py
+++ apps/programmes/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from apps.programmes.models import Programme, ProgrammeCourse
 
 
+class ProgrammeCourseInline(admin.TabularInline):
+    model = ProgrammeCourse
+    extra = 1
+    fields = ("course", "year", "period_months", "is_mandatory")
+    autocomplete_fields = ["course"]
+
+
 @admin.register(Programme)
 class ProgrammeAdmin(admin.ModelAdmin):
     list_display = ("name", "degree_type", "credits")
     search_fields = ("name", "degree_type")
     list_filter = ("degree_type",)
-    filter_horizontal = ("courses",)
-
-
-@admin.register(ProgrammeCourse)
-class ProgrammeCourseAdmin(admin.ModelAdmin):
-    list_display = ("programme", "course", "year", "period_months", "is_mandatory")
-    search_fields = ("programme__name", "course__name")
-    list_filter = ("is_mandatory", "year", "period_months")
+    inlines = [ProgrammeCourseInline]
