f6a450571800243af5b06a75daa0e221304c80ef
hajizadeh2079 <hajizadeh2079@gmail.com>
2024-12-16 13:52:39 +0100

    Add period remove some attributes

--- apps/programmes/models/programme_course.py
+++ apps/programmes/models/programme_course.py
@@ -1,4 +1,4 @@
-from django.db.models import ForeignKey, CASCADE, IntegerField, BooleanField
+from django.db.models import ForeignKey, CASCADE, PositiveIntegerField, BooleanField
 
 from apps.lib.models import UUIDModel
 
@@ -7,12 +7,8 @@ class ProgrammeCourse(UUIDModel):
     programme = ForeignKey("programmes.Programme", on_delete=CASCADE)
     course = ForeignKey("courses.Course", on_delete=CASCADE)
 
-    year = IntegerField()
-    period_months = IntegerField()
+    year = PositiveIntegerField()
     is_mandatory = BooleanField(default=False)
 
     def __str__(self):
-        return (
-            f"{self.course.name} in {self.programme.name} - "
-            f"Year {self.year}, {self.period_months} months"
-        )
+        return f"{self.course.name} in {self.programme.name} - Year {self.year}"
