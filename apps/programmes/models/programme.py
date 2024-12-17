21334377b6f2663e494eb44d1fb65bbf326670a5
hajizadeh2079 <hajizadeh2079@gmail.com>
2024-12-17 16:34:27 +0100

    Fix programme logic after removing degree_type

--- apps/programmes/models/programme.py
+++ apps/programmes/models/programme.py
@@ -14,5 +14,13 @@ class Programme(UUIDModel):
         blank=True,
     )
 
+    @property
+    def degree_type(self):
+        degrees = getattr(self, "degrees", None)
+        if degrees:
+            degree = degrees.first()
+            return degree.name if degree else None
+        return None
+
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name} - Degree: {self.degree_type or 'No Degree'}"
