c775e2455b89457be6807f6d0547deea3494e310
José Miguel Quintana <josemiguelquinta@gmail.com>
2025-01-08 15:44:40 +0100

    fix: remove auth to search programmes and courses

--- apps/programmes/views.py
+++ apps/programmes/views.py
@@ -2,10 +2,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from apps.lib.pagination import StandardPagination
 from apps.programmes.models import Programme
@@ -13,13 +11,11 @@ from apps.programmes.serializers import ProgrammeSerializer
 from apps.programmes.filters import ProgrammeFilter
 from apps.courses.serializers import CourseSerializer
 
+
 class ProgrammeViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
     queryset = Programme.objects.all()
     serializer_class = ProgrammeSerializer
     pagination_class = StandardPagination
-
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
 
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProgrammeFilter
@@ -31,7 +27,9 @@ class ProgrammeViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin):
 
         response_data = {
             "programme": ProgrammeSerializer(programme).data,
-            "courses": CourseSerializer(courses, many=True, context={"programme": programme}).data,
+            "courses": CourseSerializer(
+                courses, many=True, context={"programme": programme}
+            ).data,
         }
 
         return Response(response_data, status=status.HTTP_200_OK)
