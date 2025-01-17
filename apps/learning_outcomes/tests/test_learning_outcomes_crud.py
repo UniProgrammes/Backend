842814d195171df39a173e99bee3e2b50bd452d1
José Miguel Quintana <josemiguelquinta@gmail.com>
2025-01-17 13:38:13 +0100

    fix: tests requiring auth for open endpoints

--- apps/learning_outcomes/tests/test_learning_outcomes_crud.py
+++ apps/learning_outcomes/tests/test_learning_outcomes_crud.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from apps.users.models import User
 from apps.learning_outcomes.factories import LearningOutcomeFactory
 
+
 class TestLearningOutcomeCrud(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create_user(username="test", password="test")
@@ -26,10 +27,12 @@ class TestLearningOutcomeCrud(APITestCase):
         url = reverse("learning-outcome-list")
         self.client.force_authenticate(user=None)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data["results"]), 3)
 
     def test_retrieve_without_authentication(self):
         url = reverse("learning-outcome-detail", args=[str(self.learning_outcome.id)])
         self.client.force_authenticate(user=None)
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["id"], str(self.learning_outcome.id))
